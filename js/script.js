de17a16d7a3cdd8102ba6e57bef0f74ddf5df284
Meital Mishni <meital.mishni@gmail.com>
2022-09-05 20:58:47 +0300

    modify script file - change num

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const fibonacciNo = document.getElementById('fibonacciNo');
 const result = document.getElementById('result');
 
-const num = 10;
+const num = 8;
 fibonacciNo.innerHTML = num;
 result.innerHTML = 21;
