3637f38a3dde3948eee1e8079b07a32614752ded
Meital Mishni <meital.mishni@gmail.com>
2022-09-17 18:56:07 +0300

    milestone 4 first commit

--- js/script.js
+++ js/script.js
@@ -1,30 +1,53 @@
-let temp1;
-let temp2;
-let next;
-
-function calculateFibonacci(num) {
-    for (let i = 0; i < num; i++) {
-        next = temp1 + temp2;
-        temp1 = temp2;
-        temp2 = next;
-    }
-
-    return temp1;
-}
-
 const submit = document.getElementById('submit');
 const result = document.getElementById('result');
+const serverError = document.getElementById('server-error');
 
-function fibonacci(event) {
+const calculateFibonacci = async (url) => {
+    try {
+        const fibonacciServerResponse = await fetch(url);
+
+        if (!fibonacciServerResponse.ok) {
+            throw Error(await fibonacciServerResponse.text());
+            //throw Error('SyntaxError: Unexpected non-whitespace character after JSON at position 3');
+        }
+
+        const fibonacciServerResult = await fibonacciServerResponse.json();
+        return fibonacciServerResult;
+    }
+    catch (error) {
+        //console.log(error);
+        serverError.innerHTML = 'Server ' + error;
+    }
+}
+
+const fibonacci = async (event) => {
     event.preventDefault();
 
-    temp1 = 0;
-    temp2 = 1;
-
     const positionNum = document.getElementById('position').value;
-    const resultNum = calculateFibonacci(positionNum);
 
-    result.innerHTML = resultNum;
+    result.innerHTML = '';
+    serverError.innerHTML = '';
+
+    if (positionNum > 50) {
+        //console.log('more than 50');
+        document.getElementById('position').classList.add('error');
+        document.getElementById('error50').classList.remove('hideError');
+        submit.blur();
+        return false;
+    }
+
+    result.classList.add('spinner-border');
+    const resultNum = await calculateFibonacci(`http://localhost:5050/fibonacci/${positionNum}`);
+    result.classList.remove('spinner-border');
+
+    result.innerHTML = resultNum.result;
 }
 
 submit.addEventListener('click', fibonacci);
+
+document.getElementById('position').addEventListener('keyup', function () {
+    document.getElementById('position').classList.remove('error');
+    document.getElementById('error50').classList.add('hideError');
+    result.innerHTML = '';
+    serverError.innerHTML = '';
+})
